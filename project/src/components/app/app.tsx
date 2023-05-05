a18e24b51f40e48c1addcf38ed16f928c74213f9
Elena <eamatytsina@gmail.com>
2023-05-05 23:30:26 +0300

    [frontend]: добавлены страницы интро,  регистрация, вход,  опросник тренера и пользователя

---
+++ project/src/components/app/app.tsx
@@ -0,0 +1,18 @@
+// import IntroScreen from '../../pages/intro-screen/intro-screen';
+// import SignInScreen from '../../pages/sign-in/sing-in';
+
+import QuestionnaireCoachScreen from '../../pages/questionnaire-coach-screen/questionnaire-coach-screen';
+
+// import SignUpScreen from '../../pages/sign-up/sign-up';
+
+function App(): JSX.Element {
+  return (
+    // <IntroScreen/>
+    // <SignInScreen/>
+    // <SignUpScreen/>
+    <QuestionnaireCoachScreen/>
+
+  );
+}
+
+export default App;
